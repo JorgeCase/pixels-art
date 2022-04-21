080f713a31216625ca34ebf6a124359371d7e4a5
JorgeKz <jorgekzbra@gmail.com>
2022-04-21 17:21:39 -0300

    Implementando requisito 05

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-window.onload = function() {
-    let linhas = 5;
-    let colunas = 5;
-    let pixel = document.getElementsByClassName('pixel');
+window.onload = function () {
+  const lines = 5;
+  const columns = 5;
 
-//A função abaixo foi desenvolvida em concordância com o que foi discutido na mentoria técnica com o João Andrade Jr.
-    function criaQuadro() {
-        let paiSection = document.getElementById('pixel-board');
-        for (let index = 0; index < linhas; index += 1) {
-            let criaLinha = document.createElement('div');
-            paiSection.appendChild(criaLinha);
-            criaLinha.className = 'pixel';
-            console.log(paiSection);
-            for (let index2 = 0; index2 < colunas - 1; index2 += 1) {
-                let criaColuna = document.createElement('div');
-                criaLinha.appendChild(criaColuna);
-                criaColuna.className = 'pixel';
-                console.log(criaColuna);
-            }
-        }
+  // A função abaixo foi desenvolvida em concordância com o que foi discutido na mentoria técnica com o João Andrade Jr.
+  function createFrame() {
+    const sectionFather = document.getElementById('pixel-board');
+    for (let index = 0; index < lines; index += 1) {
+      const createLine = document.createElement('div');
+      sectionFather.appendChild(createLine);
+      createLine.className = 'linha-pixel';
+      for (let index2 = 0; index2 < columns; index2 += 1) {
+        const createColumn = document.createElement('div');
+        createLine.appendChild(createColumn);
+        createColumn.className = 'pixel';
+      }
     }
-    criaQuadro();
+  }
+  createFrame();
 };
 
-/* window.onload = function() {
-    let basePalette = 5;
-    let numberOfLines = 5
-    let controlLeft = numberOfLines; // 5
-    let controlRight = numberOfLines; // 5
-    let lines = document.querySelectorAll(".pixel");
-
-// Cria uma caixa com base nas diferentes classes
-function createBox(className) {
-    let box = document.createElement("pixel");
-    box.className = className;
-    return box;
-  }
-
- // Preenche uma linha
- function fillLine(divLine) {
-    for (let lineColumn = 1; lineColumn <= basePyramid; lineColumn += 1) {
-      if(lineColumn >= controlLeft && lineColumn <= controlRight) {
-        let box = createBox("box");
-        divLine.appendChild(box);
-      } else {
-        divLine.appendChild(createBox("box-empty"));
-      }
-    }
-  }
-  
-    // Passa por todos as linhas (div com class line) e preenche o triangulo
-    function fillTriangle(lines) {
-      for(let index = 0; index < lines.length; index += 1) {
-        fillLine(lines[index]);
-        controlRight += 1;
-        controlLeft -= 1;
-      }
-    }
-     */
+// Altera a classe color do elemento preto da paleta de cores
+/* let cor = 'black'; */
+/*
+const tamLinha = document.getElementByClassName('linha-pixel');
+tamLinha.style.maxWidth = (40 * 5);
+ */
