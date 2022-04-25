3f5a4522564341153bade58ce7f45861adc59a51
JorgeKz <jorgekzbra@gmail.com>
2022-04-25 17:46:33 -0300

    Implementando requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* window.onload = function(){ */
 const initialColor = document.getElementById('palette1');
 initialColor.style.color = 'black';
-// initialColor.className = 'selected';
+const initialLength = document.querySelector('#pixel-board');
 // A função abaixo foi desenvolvida em concordância com o que foi discutido na mentoria técnica com o João Andrade Jr.
 // Essa função cria os pixels do quadro
 function createFrame(num) {
@@ -17,22 +17,22 @@ function createFrame(num) {
     }
   }
 }
-createFrame(5);
+createFrame();
 
 // Adiciona o evento que seleciona a cor
 const clickColorSelect = document.getElementsByClassName('color');
 for (let i3 = 0; i3 < clickColorSelect.length; i3 += 1) {
-  clickColorSelect[i3].addEventListener('click', function (event) {
+  clickColorSelect[i3].addEventListener('click', (event) => {
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
   });
 }
 
 // Aplica a cor selecionada na paleta ao pixel
-function changeColor () {
+function changeColor() {
   const pixelClass = document.querySelectorAll('.pixel');
   for (let i4 = 0; i4 < pixelClass.length; i4 += 1) {
-    pixelClass[i4].addEventListener('click', function (event) {
+    pixelClass[i4].addEventListener('click', (event) => {
       const element = document.querySelector('.selected');
       const objectCss = window.getComputedStyle(element);
       const colorCss = objectCss.getPropertyValue('background-color');
@@ -42,14 +42,52 @@ function changeColor () {
 }
 changeColor();
 
-// Aplica a cor 'branca' para todos os pixels, resetando a área de pintura
+// Reseta a área dos pixels. Aplica a cor 'branca' para todos os pixels.
 const clearButton = document.getElementById('clear-board');
-const selectPixel = document.querySelectorAll('.pixel');
+
 function clearPixels() {
-  clearButton.addEventListener('click', function () {
+  clearButton.addEventListener('click', () => {
+    const selectPixel = document.querySelectorAll('.pixel');
+    console.log(selectPixel);
     for (let i5 = 0; i5 < selectPixel.length; i5 += 1) {
-      selectPixel[i5].style.backgroundColor = 'white';
+      selectPixel[i5].style.backgroundColor = 'rgb( 255, 255, 255)';
     }
   });
 }
 clearPixels();
+
+// Escolhendo o tamanho do quadro de pixels
+// Criando constantes
+const inputNum = document.createElement('input');
+const buttonNum = document.createElement('button');
+const divFather = document.querySelector('#choiceNumber');
+// Inserindo características
+inputNum.id = 'board-size';
+inputNum.min = 1;
+inputNum.type = 'number';
+buttonNum.id = 'generate-board';
+buttonNum.innerText = 'VQV';
+// Criando os elementos na página
+divFather.appendChild(inputNum);
+divFather.appendChild(buttonNum);
+// Estabelecendo condições
+function choice() {
+  createFrame(5);
+  changeColor();
+  buttonNum.addEventListener('click', () => {
+    if (inputNum.value === '') {
+      alert('Board inválido!');
+      return;
+    }
+    if (inputNum.value < 5) {
+      inputNum.value = 5;
+    }
+    if (inputNum.value > 50) {
+      inputNum.value = 50;
+    }
+    initialLength.textContent = '';
+    createFrame(inputNum.value);
+    changeColor();
+  });
+}
+choice();
